8558616ed3b667eb378f6a5244dbab8217bb5788
Syeda Samana Zehra <samanazehra78692@gmail.com>
2025-05-13 00:06:28 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function calculater() {
     <div className="calculator">
       <h1 className='text-4xl text-orange-500 uppercase items-center justify-center font-mono mb-4 animate-bounce'> Calculator</h1>
       <div className=' mx-auto items-center justify-center w-[380px] h-[550px] bg-orange-950 rounded-2xl shadow-[0_0_25px_rgba(255,100,0,0.5)] p-4 border border-orange-500'>
-        <div className="w-80 text-white bg-orange-950 mb-4 mt-5 rounded-2xl shadow-[0_0_25px_rgba(255,100,0,0.5)] p-4 border border-orange-500">{input || "0"}</div>
+        <div className="w-80 text-white bg-orange-950 mb-4 mr-2 mt-5 rounded-2xl shadow-[0_0_25px_rgba(255,100,0,0.5)] p-4 border border-orange-500">{input || "0"}</div>
         <div className='grid grid-cols-4 gap-3 '>
           {buttons.map((val) => (
 
